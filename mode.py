12b27101fc9a5c9bde749d2ce01cf9abf8d08133
aathmika10 <65343462+aathmika10@users.noreply.github.com>
2021-06-22 13:16:10 +0530

    Add files via upload

--- mode.py
+++ mode.py
@@ -13,17 +13,39 @@ for i in range(len(data)):
 
 count=Counter(newData)
 modeDataForRange={
-    "50-60":0,
-    "60-70":0,
-    "70-80":0
-}
+    "75-85":0,
+    "85-95":0,
+    "95-105":0,
+    "105-115":0,
+    "115-125":0,
+    "125-135":0,
+    "135-145":0,
+    "145-155":0,
+    "155-165":0,
+    "165-175":0
+    }
 for height,occurence in count.items():
-    if 50<float(height)<60:
-        modeDataForRange["50-60"]+=occurence
-    elif 60<float(height)<70:
-        modeDataForRange["60-70"]+=occurence
-    elif 70<float(height<80):
-        modeDataForRange["70-80"]+=occurence
+    if 75<float(height)<85:
+        modeDataForRange["75-85"]+=occurence
+    elif 85<float(height)<95:
+        modeDataForRange["85-95"]+=occurence
+    elif 95<float(height)<105:
+        modeDataForRange["95-105"]+=occurence
+    elif 105<float(height)<115:
+        modeDataForRange["105-115"]+=occurence
+    elif 115<float(height)<125:
+        modeDataForRange["115-125"]+=occurence
+    elif 125<float(height)<135:
+        modeDataForRange["125-135"]+=occurence
+    elif 135<float(height)<145:
+        modeDataForRange["135-145"]+=occurence
+    elif 145<float(height)<155:
+        modeDataForRange["145-155"]+=occurence
+    elif 155<float(height)<165:
+        modeDataForRange["155-165"]+=occurence
+    elif 165<float(height)<175:
+        modeDataForRange["165-175"]+=occurence
+    
 
     modeRange,modeOccurence=0,0
     for range,occurence in modeDataForRange.items():
